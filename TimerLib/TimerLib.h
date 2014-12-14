783044e990d426dab5f1b57e2cd4615f43a51f87
Yasuaki Nakazawa <yasuaki.nakazawa@myu-on.co.jp>
2014-12-14 19:00:07 +0900

    add -> static library project

---
+++ TimerLib/TimerLib.h
@@ -0,0 +1,13 @@
+//
+//  TimerLib.h
+//  TimerLib
+//
+//  Created by myuon on 2014/12/14.
+//  Copyright (c) 2014年 yasu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TimerLib : NSObject
+
+@end
